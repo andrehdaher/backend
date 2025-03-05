b2ccd34117ca50f4a4757266dc1e6f6e7bed9e19
andrehdaher <andrehdaher2003@gmail.com>
2025-03-05 14:10:36 +0300

    Initial commit

--- models/paymentcompany.js
+++ models/paymentcompany.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const paymentSchema1 = new mongoose.Schema({
-  number: String,
-  speed: String,
-  paidAmount: String,
-  requiredAmount: String,
-  company: String,
-  provider: String,
-});
+  number: { type: String, required: true }, // إذا كانت القيمة مطلوبة
+  speed: { type: String, required: true },  // إذا كانت القيمة مطلوبة
+  paidAmount: { type: Number, required: true },  // تغيير إلى Number إذا كانت المبالغ المالية
+  requiredAmount: { type: Number, required: true },  // تغيير إلى Number
+  company: { type: String, required: true },
+  provider: { type: String, required: true },
+}, { collation: { locale: 'ar' } });  // استخدام collation مع الـ schema (اختياري)
 
 const Paymentcompany = mongoose.model('Paymentcompany', paymentSchema1);
 
